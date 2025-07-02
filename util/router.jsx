8c8495d669981fee72da551ff27c7564e03d860e
Erlend01-Kodehode <erlend01.kodehode@gmail.com>
2025-07-02 14:57:22 +0200

    rout and dat

--- util/router.jsx
+++ util/router.jsx
@@ -1,9 +1,33 @@
-import { createHashRouter } from "react-router-dom";
+import { createHashRouter, Navigate } from "react-router-dom";
 import App from "../src/App";
+import Main from "../public/pages/Main";
+import Factions from "../public/pages/Factions";
+import FactionPage from "../public/pages/FactionPage";
 
 const router = createHashRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Navigate />,
+      },
+      {
+        path: "home",
+        element: <Main />,
+      },
+      {
+        path: "faction",
+        element: <Factions />,
+        children: [
+          {
+            path: ":faction",
+            element: <FactionPage data={faction} />,
+          },
+        ],
+      },
+    ],
   },
 ]);
