8677475bd4a9a9d189b454246b2863a61af8bf98
Erlend01-Kodehode <erlend01.kodehode@gmail.com>
2025-07-02 09:36:05 +0200

    stuff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { base_url } from "../GameProjects/multiplayerGameCollection/FrontEnd/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: base_url,
+});
